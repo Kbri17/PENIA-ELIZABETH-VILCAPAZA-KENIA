112479642ef6b3eed29791309d7bbb45e1938b08
Elizabeth <lesep.28@gmail.com>
2024-08-12 19:25:14 -0500

    refactor impl

--- src/dao/impl/DaoH2Odontologo.java
+++ src/dao/impl/DaoH2Odontologo.java
@@ -2,9 +2,9 @@ package dao.impl;
 
 import org.apache.log4j.Logger;
 
-public class DaoH2 {
+public class DaoH2Odontologo {
 
-    public static final Logger logger = Logger.getLogger(DaoH2.class);
+    public static final Logger logger = Logger.getLogger(DaoH2Odontologo.class);
     public static final String INSERT = "INSERT INTO PACIENTES VALUES(DEFAULT,?,?,?,?,? )";
     public static final String SELECT_ID = "SELECT * FROM PACIENTES WHERE ID = ?";
 

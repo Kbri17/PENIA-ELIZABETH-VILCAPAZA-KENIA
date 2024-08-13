69bf66956d57858b199f31d04d5fbcad3370d82f
Kbri17 <104737155+Kbri17@users.noreply.github.com>
2024-08-12 19:50:45 -0500

    metodo guardar hecho

--- src/dao/impl/DaoOdontologo.java
+++ src/dao/impl/DaoOdontologo.java
@@ -1,8 +1,12 @@
 package dao.impl;
 
 import dao.IDao;
+import db.H2Connection;
 import model.Odontologo;
 import org.apache.log4j.Logger;
+
+import java.sql.*;
+import java.util.List;
 
 public class DaoOdontologo implements IDao<Odontologo> {
 
@@ -12,4 +16,57 @@ public class DaoOdontologo implements IDao<Odontologo> {
     public static final String INSERT = "INSERT INTO ODONTOLOGOS VALUES(DEFAULT,?,?,? )";
     public static final String SELECT_ALL = "SELECT * FROM ODONTOLOGOS";
 
+    @Override
+    public Odontologo guardar(Odontologo odontologo) {
+
+        Connection connection = null;
+        Odontologo odontologoARetornar = null;
+        try{
+            connection = H2Connection.getConnection();
+            connection.setAutoCommit(false);
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, odontologo.getNroMatricula());
+            preparedStatement.setString(2, odontologo.getNombre());
+            preparedStatement.setString(3, odontologo.getApellido());
+            preparedStatement.executeUpdate();
+            connection.commit();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            while (resultSet.next()){
+                Integer id = resultSet.getInt(1);
+                odontologoARetornar = new Odontologo(id, odontologo.getNroMatricula(),odontologo.getNombre(),odontologo.getApellido());
+            }
+            logger.info("odontologo "+ odontologoARetornar);
+
+        }catch (Exception e){
+            if(connection != null){
+                try {
+                    connection.rollback();
+                } catch (SQLException ex) {
+                    logger.error(e.getMessage());
+                } finally {
+                    try {
+                        connection.setAutoCommit(true);
+                    } catch (SQLException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            }
+            logger.error(e.getMessage());
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                logger.error(e.getMessage());
+                e.printStackTrace();
+            }
+        }
+        return odontologoARetornar;
+
+    }
+
+    @Override
+    public List<Odontologo> listarTodos() {
+        return null;
+    }
 }
